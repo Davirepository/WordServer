b3f6d09a05b7fba92568b46671c8493791f1220d
David <bolotaev-david@mail.ru>
2019-12-07 22:01:00 +0300

    add xcodeproj

--- wordsServer/Package.swift
+++ wordsServer/Package.swift
@@ -2,13 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "apple-pie",
+    name: "get-word",
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
         .package(url: "https://github.com/vapor/leaf.git", .upToNextMinor(from: "3.0.0")),
-//        .package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMinor(from: "3.0.0")),
-//        .package(url: "https://github.com/vapor/fluent-mysql.git", .upToNextMinor(from: "3.0.0")),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", .upToNextMinor(from: "1.0.0")),
         .package(url: "https://github.com/vapor/crypto.git", .upToNextMinor(from: "3.3.0")),
     ],
